fb767b50f1513c39d378af1e3431ae0aeeeb9ccf
ho-ji <wkdd0205@gmail.com>
2024-12-30 17:55:18 +0900

    feat: nestJS 서버 시작

---
+++ README.md
@@ -0,0 +1,7 @@
+# Cook Log 서버
+
+- nestJS서버
+
+## DB설계
+
+https://www.erdcloud.com/d/NrLCTLwFtSBZXDSsC
---
+++ src/app.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { UserService } from './user/user.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        synchronize: true,
+      }),
+    }),
+    UserModule,
+  ],
+  controllers: [AppController, UserController],
+  providers: [AppService, UserService],
+})
+export class AppModule {}
