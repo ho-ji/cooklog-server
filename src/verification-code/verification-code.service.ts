2d721fb865e81cc6b5d49577f52518662e61f07f
ho-ji <wkdd0205@gmail.com>
2025-01-05 01:38:01 +0900

    feat: 인증 10회 이상 시도 시 실패 추가

--- src/verification-code/verification-code.service.ts
+++ src/verification-code/verification-code.service.ts
@@ -12,11 +12,17 @@ export class VerificationCodeService {
   async saveVerificationCode(email: string): Promise<string> {
     const code = codeGenerator();
     const ttl = 180;
-    await this.redis.del(email);
-    await this.redis.setex(email, ttl, code);
+    await this.redis.del(`verification:${email}`);
+    await this.redis.del(`attempts:${email}`);
+    await this.redis.setex(`verification:${email}`, ttl, code);
+    await this.redis.setex(`attempts:${email}`, ttl, 10);
     return code;
   }
   async getVerificationCode(email: string): Promise<string | null> {
-    return await this.redis.get(email);
+    const attempts = await this.redis.get(`attempts:${email}`);
+    if (!attempts || parseInt(attempts) <= 0) return null;
+    const ttl = await this.redis.ttl(`attempts:${email}`);
+    await this.redis.setex(`attempts:${email}`, ttl, parseInt(attempts) - 1);
+    return await this.redis.get(`verification:${email}`);
   }
 }
